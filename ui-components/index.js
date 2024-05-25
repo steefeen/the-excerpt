bfee0d669dc1c7ea0f8d6c494960547950421c24
Stefan Englmeier <stefan.englmeier@hm.edu>
2024-05-25 14:36:34 +0200

    update

---
+++ ui-components/index.js
@@ -0,0 +1,6 @@
+export { default as FieldCreateForm } from "./FieldCreateForm";
+export { default as FieldUpdateForm } from "./FieldUpdateForm";
+export { default as PostCreateForm } from "./PostCreateForm";
+export { default as PostUpdateForm } from "./PostUpdateForm";
+export { default as FolderCreateForm } from "./FolderCreateForm";
+export { default as FolderUpdateForm } from "./FolderUpdateForm";
