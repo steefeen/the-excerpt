b8a629181bfe76e86e0c848de4e2e237946e469e
Stefan Englmeier <stefan.englmeier@hm.edu>
2024-05-25 13:46:21 +0200

    update

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -1,16 +1,30 @@
-import { useRouteError } from "react-router-dom";
+import {isRouteErrorResponse, useRouteError} from "react-router-dom";
+
+interface RouteError {
+    data: string;
+    error: {
+        columnNumber: number;
+        fileName: string;
+        lineNumber: number;
+        message: string;
+        stack: string;
+    };
+    internal: boolean;
+    status: number;
+    statusText: string;
+}
 
 export default function ErrorPage() {
-    const error = useRouteError();
+    const error = useRouteError() as RouteError;
     console.error(error);
 
-    // @ts-expect-error
+
     return (
         <div id="error-page">
             <h1>Oops!</h1>
             <p>Sorry, an unexpected error has occurred.</p>
             <p>
-                <i>{error.statusText || error.message}</i>
+                <i>{error.statusText || error.error.message}</i>
             </p>
         </div>
     );
